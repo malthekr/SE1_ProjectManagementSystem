ab5a06410b88c44923ae5b253b8ba37405dedb29
N. A. K <niklas.kjolbro@live.com>
2023-05-08 11:12:32 +0200

    Added exception that ID can't contain special characters or whitespaces

--- projectmanagement01/src/main/java/softwarehuset/management/app/EmployeeRepository.java
+++ projectmanagement01/src/main/java/softwarehuset/management/app/EmployeeRepository.java
@@ -17,12 +17,25 @@ public class EmployeeRepository {
 	
 	// Add an employee to repository
 	public void addEmployee(Employee employee) throws OperationNotAllowedException {
+		
+		//Check if ID alreadt exists
 		if(checkIfEmployeeExists(employee.getId())) {
 			throw new OperationNotAllowedException("Employee ID already taken");
 		} 
 		
+		//Check if ID is too long
 		if(employee.getId().length() > 4 || employee.getId().length() < 1){
 			throw new OperationNotAllowedException("Employee ID is too long");
+		}
+		
+		//Check if ID has spaces in it:
+		if(employee.getId().contains(" ")){
+			throw new OperationNotAllowedException("Employee ID can't contain whitespaces");
+		}
+		
+		//Check if special characters are used:
+		if(!employee.getId().matches("[a-zæøåA-ZÆØÅ]+")){
+			throw new OperationNotAllowedException("Employee ID can't contain special characters");
 		}
 		
 		employeeRepository.add(employee);
